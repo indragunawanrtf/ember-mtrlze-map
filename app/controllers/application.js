80c4d42d12693f079732e5d8d3057f44f00da910
bernard <bernamaxim@gmail.com>
2018-01-24 20:38:59 +0700

    Center Map on Indonesian region

---
+++ app/controllers/application.js
@@ -0,0 +1,7 @@
+import Controller from '@ember/controller';
+
+export default Controller.extend({
+  lat: '-1.6281701', //Indonesian Latitide
+  lng:'119.8490847', //Indonesian Longitude
+  zoom: 4
+});
